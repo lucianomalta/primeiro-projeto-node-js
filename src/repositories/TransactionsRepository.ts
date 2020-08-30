b44929210f38d25821bb44f5c185c0861b1ab584
Luciano Vasconcelos Malta <luciano@orgsystem.com.br>
2020-08-30 10:52:55 -0300

    Correções do desafio 5

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            acumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+        return acumulator;
+      }, {
+        income: 0,
+        outcome: 0,
+        total: 0
+      }
+    )
+    balance.total = balance.income - balance.outcome;
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create( { title, type, value }:CreateTransactionDTO ): Transaction {
+
+    const { total } = this.getBalance();
+
+    if (type == 'outcome' && total < value) {
+      throw Error('total is insuficient!');
+    }
+    console.log({total, value})
+    const transation = new Transaction({title, type, value});
+
+    this.transactions.push(transation);
+    return transation;
   }
 }
 
